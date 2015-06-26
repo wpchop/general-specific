1fb8e78b6d605716208af0a346df2015d88d7228
Bridget O'Daniel <odanielb@berea.edu>
2015-06-26 15:49:33 +0000

    Takes in user entered paths and filenames

--- CoreNLP/make_filelist.py
+++ CoreNLP/make_filelist.py
@@ -1,5 +1,12 @@
 import os
-path = "/nlp/users/odanielb/nyt_idf/texts"
-with open("input/filelist.txt", "w") as outFile:
+
+# Example paths:
+#   to unstructured PubMed abstracts: "/nlp/users/odanielb/pubmed_idf/texts"
+#   to NYT data: "/nlp/users/odanielb/nyt_idf/texts"
+
+path = raw_input("Enter the path to the directory containing the files to list:\n")
+outfile = raw_input("Enter the desired name for the output list (extension included):\n")
+
+with open(outfile, "w") as outFile:
   for file in os.listdir(path):
     outFile.write(path+'/'+file+'\n')
